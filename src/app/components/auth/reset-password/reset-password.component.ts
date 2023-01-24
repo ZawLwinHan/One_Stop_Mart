377a0cb1fe8f0409b0ffe062ed5893f4ac787587
Ei Myat <74184859+eimyat528@users.noreply.github.com>
2023-01-24 10:21:27 +0630

    login

--- src/app/components/auth/reset-password/reset-password.component.ts
+++ src/app/components/auth/reset-password/reset-password.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-reset-password',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordComponent implements OnInit {
 
-  constructor() { }
+  password: any;
+  show = false;
+
+
+  showPassword = false;
+  login_form!: FormGroup;
+  hide = true;
+  constructor( private route: Router,private _formBuilder: FormBuilder, private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
+    this.login_form = this._formBuilder.group({
+      user_name: ["", Validators.required],
+      password: ["", Validators.required],
+    });
+    this.password = 'password';
   }
 
+  login(){
+    // if (this.login_form.invalid) {
+    //   this._snackBar.open('Username and Password required', 'Ok', {
+    //     duration: 3000,
+    //   })
+    // }
+    // else {
+    //   this.route.navigate(['/admin/dashboard']);
+    // }
+  }
+
+  forgotPassword(){
+    this.route.navigate(['/login/forgotPassword']);
+  }
+
+  onClick() {
+    if (this.password === 'password') {
+      this.password = 'text';
+      this.show = true;
+    } else {
+      this.password = 'password';
+      this.show = false;
+    }
+  }
 }
+
